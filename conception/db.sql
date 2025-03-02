b0ef4d29abc783b2dde72b695469ba54b964f504
LucasBenaivo <rabenaivolucas@gmail.com>
2025-03-02 17:30:22 +0300

    base

--- conception/db.sql
+++ conception/db.sql
@@ -1,59 +1,78 @@
-create table role (
-    id serial primary key ,
-    valeur varchar(250)UNIQUE NOT NULL unique not null
-);
-insert into role (valeur) values ('User');
-insert into role (valeur) values ('Admin');
+CREATE SEQUENCE role_id_seq START 1;
+CREATE SEQUENCE utilisateur_id_seq START 1;
+CREATE SEQUENCE avion_id_seq START 1;
+CREATE SEQUENCE vol_id_seq START 1;
+CREATE SEQUENCE reservation_id_seq START 1;
+CREATE SEQUENCE continent_id_seq START 1;
+CREATE SEQUENCE ville_id_seq START 1;
+CREATE SEQUENCE type_siege_id_seq START 1;
+CREATE SEQUENCE modele_id_seq START 1;
 
-create table utilisateur (
-    id serial primary key,
-    nom varchar(250) not null ,
-    email varchar(250) not null ,
-    mdp varchar(250) not null ,
-    id_role int references role(id)
+CREATE TABLE role (
+    id VARCHAR(6) PRIMARY KEY DEFAULT CONCAT('RLE', LPAD(nextval('role_id_seq')::TEXT, 3, '0')),
+    valeur VARCHAR(250) UNIQUE NOT NULL
 );
-insert into utilisateur(nom, email, mdp, id_role) values ('Rabenaivo', 'rabenaivolucas@gmail.com', md5('rabenaivo'), 2);
-insert into utilisateur(nom, email, mdp, id_role) values ('Ralison', 'ralison@gmail.com', md5('ralison'), 1);
+INSERT INTO role (valeur) VALUES ('User');
+INSERT INTO role (valeur) VALUES ('Admin');
+
+CREATE TABLE utilisateur (
+    id VARCHAR(6) PRIMARY KEY DEFAULT CONCAT('USR', LPAD(nextval('utilisateur_id_seq')::TEXT, 3, '0')),
+    nom VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    mdp VARCHAR(250) NOT NULL,
+    id_role VARCHAR(6) REFERENCES role(id)
+);
+INSERT INTO utilisateur(nom, email, mdp, id_role)
+VALUES
+    ('Rabenaivo', 'rabenaivolucas@gmail.com', md5('rabenaivo'), 'RLE002'),
+    ('Ralison', 'ralison@gmail.com', md5('ralison'), 'RLE001');
 
 CREATE TABLE continent (
-    id serial primary key ,
+    id VARCHAR(6) PRIMARY KEY DEFAULT CONCAT('CNT', LPAD(nextval('continent_id_seq')::TEXT, 3, '0')),
     nom VARCHAR(50) NOT NULL
 );
-INSERT INTO continent (nom_continent) VALUES
+INSERT INTO continent (nom)
+VALUES
     ('Afrique'),
     ('Amérique'),
     ('Asie'),
     ('Europe');
 
+
 CREATE TABLE ville_desservie (
-    id serial PRIMARY KEY,
+    id VARCHAR(6) PRIMARY KEY DEFAULT CONCAT('VIL', LPAD(nextval('ville_id_seq')::TEXT, 3, '0')),
     nom VARCHAR(100) NOT NULL,
-    id_continent INT,
+    id_continent VARCHAR(6),
     FOREIGN KEY (id_continent) REFERENCES continent(id)
 );
-INSERT INTO ville (nom, id_continent) VALUES
-    ('Paris', 4),
-    ('Londres', 4),
-    ('New York', 2),
-    ('Tokyo', 3),
-    ('São Paulo', 2),
-    ('Le Caire', 1);
+INSERT INTO ville_desservie (nom, id_continent)
+VALUES
+    ('Paris', 'CNT004'),
+    ('Londres', 'CNT004'),
+    ('New York', 'CNT002'),
+    ('Tokyo', 'CNT003'),
+    ('São Paulo', 'CNT002'),
+    ('Le Caire', 'CNT001');
+
 
 CREATE TABLE type_siege (
-    id serial PRIMARY KEY,
-    nom VARCHAR(50) NOT NULL,
+    id VARCHAR(6) PRIMARY KEY DEFAULT CONCAT('TSI', LPAD(nextval('type_siege_id_seq')::TEXT, 3, '0')),
+    nom VARCHAR(50) NOT NULL
 );
-INSERT INTO type_siege (nom, description) VALUES
-    ('Économique' ),
+INSERT INTO type_siege (nom)
+VALUES
+    ('Économique'),
     ('Affaires'),
     ('Première classe'),
     ('Premium Économique');
 
-create table modele (
-    id  serial primary key ,
+
+CREATE TABLE modele (
+    id VARCHAR(6) PRIMARY KEY DEFAULT CONCAT('MOD', LPAD(nextval('modele_id_seq')::TEXT, 3, '0')),
     nom VARCHAR(250)
 );
-INSERT INTO modele (nom) VALUES
+INSERT INTO modele (nom)
+VALUES
     ('Airbus A320'),
     ('Boeing 737'),
     ('Airbus A380'),
@@ -65,123 +84,119 @@ INSERT INTO modele (nom) VALUES
     ('Airbus A321'),
     ('Boeing 767');
 
-create table avion (
-    id serial primary key,
-    id_modele int references modele(id),
-    date_fabrication date
+
+CREATE TABLE avion (
+    id VARCHAR(6) PRIMARY KEY DEFAULT CONCAT('AVN', LPAD(nextval('avion_id_seq')::TEXT, 3, '0')),
+    id_modele VARCHAR(6) REFERENCES modele(id),
+    date_fabrication DATE
 );
-insert into avion (id_modele, date_fabrication) values
-    (1, '2020-05-01'),
-    (2, '2018-09-15'),  -- Boeing 737
-    (3, '2019-01-12'),  -- Airbus A380
-    (4, '2017-03-10');  -- Boeing 777
+INSERT INTO avion (id_modele, date_fabrication)
+VALUES
+    ('MOD001', '2020-05-01'),
+    ('MOD002', '2018-09-15'),
+    ('MOD003', '2019-01-12'),
+    ('MOD004', '2017-03-10');
+
 
 CREATE TABLE avion_details_type_siege (
-    id_type_siege int REFERENCES type_siege(id),
-    id_avion int REFERENCES avion(id),
-    nombre_place int,
+    id_type_siege VARCHAR(6) REFERENCES type_siege(id),
+    id_avion VARCHAR(6) REFERENCES avion(id),
+    nombre_place INT,
     PRIMARY KEY (id_type_siege, id_avion)
 );
-insert into avion_details_type_siege (id_type_siege, id_avion, nombre_place) values
+
+INSERT INTO avion_details_type_siege (id_type_siege, id_avion, nombre_place)
+VALUES
 -- Airbus A320
-(1, 1, 150),  -- Économique pour Airbus A320
-(2, 1, 20),   -- Affaires pour Airbus A320
-(3, 1, 5),    -- Première classe pour Airbus A320
-(4, 1, 5),    -- Premium Économique pour Airbus A320
+('TSI001', 'AVN001', 150),
+('TSI002', 'AVN001', 20),
+('TSI003', 'AVN001', 5),
+('TSI004', 'AVN001', 5),
 
 -- Boeing 737
-(1, 2, 160),  -- Économique pour Boeing 737
-(2, 2, 15),   -- Affaires pour Boeing 737
-(3, 2, 8),    -- Première classe pour Boeing 737
-(4, 2, 7),    -- Premium Économique pour Boeing 737
+('TSI001', 'AVN002', 160),
+('TSI002', 'AVN002', 15),
+('TSI003', 'AVN002', 8),
+('TSI004', 'AVN002', 7),
 
 -- Airbus A380
-(1, 3, 300),  -- Économique pour Airbus A380
-(2, 3, 60),   -- Affaires pour Airbus A380
-(3, 3, 15),   -- Première classe pour Airbus A380
-(4, 3, 25),   -- Premium Économique pour Airbus A380
+('TSI001', 'AVN003', 300),
+('TSI002', 'AVN003', 60),
+('TSI003', 'AVN003', 15),
+('TSI004', 'AVN003', 25),
 
 -- Boeing 777
-(1, 4, 250),  -- Économique pour Boeing 777
-(2, 4, 50),   -- Affaires pour Boeing 777
-(3, 4, 12),   -- Première classe pour Boeing 777
-(4, 4, 20);   -- Premium Économique pour Boeing 777
-
+('TSI001', 'AVN004', 250),
+('TSI002', 'AVN004', 50),
+('TSI003', 'AVN004', 12),
+('TSI004', 'AVN004', 20);
 
 
 CREATE TABLE vol (
-    id serial PRIMARY KEY,
-    id_avion int REFERENCES avion(id),
-    id_ville_depart int REFERENCES ville(id),
-    id_ville_arrivee int REFERENCES ville(id),
-    date_depart timestamp,
-    date_arrivee timestamp
+    id VARCHAR(6) PRIMARY KEY DEFAULT CONCAT('VOL', LPAD(nextval('vol_id_seq')::TEXT, 3, '0')),
+    id_avion VARCHAR(6) REFERENCES avion(id),
+    id_ville_depart VARCHAR(6) REFERENCES ville_desservie(id),
+    id_ville_arrivee VARCHAR(6) REFERENCES ville_desservie(id),
+    date_depart TIMESTAMP,
+    date_arrivee TIMESTAMP
 );
-insert into vol (id_avion, id_ville_depart, id_ville_arrivee, date_depart, date_arrivee) values
-                                                                                             (1, 1, 2, '2025-03-15 08:00:00', '2025-03-15 10:30:00'),  -- Airbus A320 (Paris -> Londres)
-                                                                                             (2, 2, 3, '2025-03-16 09:00:00', '2025-03-16 12:00:00'),  -- Boeing 737 (Londres -> New York)
-                                                                                             (3, 3, 4, '2025-03-17 14:00:00', '2025-03-17 18:00:00'),  -- Airbus A380 (New York -> Tokyo)
-                                                                                             (4, 4, 5, '2025-03-18 16:00:00', '2025-03-18 20:00:00');  -- Boeing 777 (Tokyo -> São Paulo)
-
+INSERT INTO vol (id_avion, id_ville_depart, id_ville_arrivee, date_depart, date_arrivee)
+VALUES
+    ('AVN001', 'VIL004', 'VIL004', '2025-03-15 08:00:00', '2025-03-15 10:30:00'),
+    ('AVN002', 'VIL004', 'VIL002', '2025-03-16 09:00:00', '2025-03-16 12:00:00'),
+    ('AVN003', 'VIL002', 'VIL003', '2025-03-17 14:00:00', '2025-03-17 18:00:00'),
+    ('AVN004', 'VIL003', 'VIL005', '2025-03-18 16:00:00', '2025-03-18 20:00:00');
 
 CREATE TABLE vol_type_siege (
-    id_vol int REFERENCES vol(id),
-    id_type_siege int REFERENCES type_siege(id),
-    prix DECIMAL(10, 2),
-    PRIMARY KEY (id_vol, id_type_siege)
+                                id_vol VARCHAR(6) REFERENCES vol(id),
+                                id_type_siege VARCHAR(6) REFERENCES type_siege(id),
+                                prix DECIMAL(10, 2),
+                                PRIMARY KEY (id_vol, id_type_siege)
 );
-insert into vol_type_siege (id_vol, id_type_siege, prix) values
--- Airbus A320
-(1, 1, 150.00),  -- Économique
-(1, 2, 250.00),  -- Affaires
-(1, 3, 500.00),  -- Première classe
-(1, 4, 350.00),  -- Premium Économique
+INSERT INTO vol_type_siege (id_vol, id_type_siege, prix)
+VALUES
+    ('VOL001', 'TSI001', 150.00),
+    ('VOL001', 'TSI002', 250.00),
+    ('VOL001', 'TSI003', 500.00),
+    ('VOL001', 'TSI004', 350.00),
 
--- Boeing 737
-(2, 1, 160.00),  -- Économique
-(2, 2, 220.00),  -- Affaires
-(2, 3, 450.00),  -- Première classe
-(2, 4, 330.00),  -- Premium Économique
+    ('VOL002', 'TSI001', 160.00),
+    ('VOL002', 'TSI002', 220.00),
+    ('VOL002', 'TSI003', 450.00),
+    ('VOL002', 'TSI004', 330.00),
 
--- Airbus A380
-(3, 1, 180.00),  -- Économique
-(3, 2, 280.00),  -- Affaires
-(3, 3, 600.00),  -- Première classe
-(3, 4, 400.00),  -- Premium Économique
+    ('VOL003', 'TSI001', 180.00),
+    ('VOL003', 'TSI002', 280.00),
+    ('VOL003', 'TSI003', 600.00),
+    ('VOL003', 'TSI004', 400.00),
 
--- Boeing 777
-(4, 1, 170.00),  -- Économique
-(4, 2, 270.00),  -- Affaires
-(4, 3, 550.00),  -- Première classe
-(4, 4, 380.00);  -- Premium Économique
-
+    ('VOL004', 'TSI001', 170.00),
+    ('VOL004', 'TSI002', 270.00),
+    ('VOL004', 'TSI003', 550.00),
+    ('VOL004', 'TSI004', 380.00);
 
 
 CREATE TABLE reservation (
-    id serial PRIMARY KEY,
-    id_utilisateur int REFERENCES utilisateur(id),
-    id_vol int REFERENCES vol(id),
-    date_reservation timestamp
+    id VARCHAR(6) PRIMARY KEY DEFAULT CONCAT('RES', LPAD(nextval('reservation_id_seq')::TEXT, 3, '0')),
+    id_utilisateur VARCHAR(6) REFERENCES utilisateur(id),
+    id_vol VARCHAR(6) REFERENCES vol(id),
+    date_reservation TIMESTAMP
 );
-insert into reservation (id_utilisateur, id_vol, date_reservation) values
-                                                                       (1, 1, '2025-03-10 09:00:00'),  -- Rabenaivo réserve un vol Paris -> Londres
-                                                                       (2, 2, '2025-03-10 09:30:00');  -- Ralison réserve un vol Londres -> New York
+INSERT INTO reservation (id_utilisateur, id_vol, date_reservation)
+VALUES
+    ('USR001', 'VOL001', '2025-03-10 09:00:00'),
+    ('USR002', 'VOL002', '2025-03-10 09:30:00');
 
 
 CREATE TABLE reservation_details (
-     id_reservation int REFERENCES reservation(id),
-     id_type_siege int REFERENCES type_siege(id),
-     nombre_places int,
-     PRIMARY KEY (id_reservation, id_type_siege)
+    id_reservation VARCHAR(6) REFERENCES reservation(id),
+    id_type_siege VARCHAR(6) REFERENCES type_siege(id),
+    nombre_places INT,
+    PRIMARY KEY (id_reservation, id_type_siege)
 );
-insert into reservation_details (id_reservation, id_type_siege, nombre_places) values
--- Rabenaivo réserve un vol Paris -> Londres
-(1, 1, 2),  -- 2 places en Économique
-(1, 2, 1),  -- 1 place en Affaires
-
--- Ralison réserve un vol Londres -> New York
-(2, 1, 3),  -- 3 places en Économique
-(2, 3, 1);  -- 1 place en Première classe
-
-
-
+INSERT INTO reservation_details (id_reservation, id_type_siege, nombre_places)
+VALUES
+    ('RES001', 'TSI001', 2),
+    ('RES001', 'TSI002', 1),
+    ('RES002', 'TSI001', 3),
+    ('RES002', 'TSI003', 1);
